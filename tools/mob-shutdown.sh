ab27c4336c85f9d56b06882548df6f304f22b234
Dmitry Rendov <drendov@gmail.com>
2026-01-11 15:46:13 +0000

    87 - Adjust SYN flood protection, enable logs

---
+++ tools/mob-shutdown.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo systemctl stop survival &&
+sudo systemctl stop vanilla &&
+sudo systemctl stop creative &&
+sudo systemctl stop lobby &&
+sudo systemctl stop bungeecord
+
+exit 0
